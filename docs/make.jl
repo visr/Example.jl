f67936f09262deeb62626af1c13abbe8d020da03
Martijn Visser <mgvisser@gmail.com>
2019-03-03 12:05:36 +0100

    switch to Algolia search

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,11 @@
 using Documenter, Example
 
-makedocs(modules = [Example], sitename = "Example.jl")
+makedocs(
+    modules = [Example],
+    sitename = "Example.jl",
+    format = Documenter.HTML(search="algolia"),
+    # format = Documenter.HTML(search="lunr"),
+)
 
 deploydocs(
     repo = "github.com/visr/Example.jl.git",
